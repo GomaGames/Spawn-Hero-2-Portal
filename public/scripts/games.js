e78c1ec58388ae7992b598d72bf57af3d2b2b9e0
Jon Borgonia <jon@gomagames.com>
2016-05-05 20:30:16 -1000

    add game listing

---
+++ public/scripts/games.js
@@ -0,0 +1,38 @@
+(function(window) {
+
+  var ref = new Firebase("https://spawn-hero-2.firebaseio.com").child("games");
+
+  ref.on("value", function(snapshot) {
+    var games = snapshot.val();
+    $("#games-list").html(Object.keys(games).map(function(game){
+      return $("<div>", {
+        "class" : "column",
+        html : $("<div>", {
+          "class" : "game",
+          html : $("<div>", {
+            "class" : "game-inner",
+            html : [
+              $("<h2>", {
+                html : games[game].title
+              }),
+              $("<h3>", {
+                html : games[game].developer
+              }),
+              $("<a>", {
+                href : "#",
+                "class" : "play-button",
+                html : [
+                  $("<i>", {
+                    "class" : "fa fa-gamepad"
+                  }),
+                  "Play Game"
+                ]
+              })
+            ]
+          })
+        })
+      });
+    }));
+  });
+
+}(window));
